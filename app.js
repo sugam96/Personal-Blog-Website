63f1a3230a5cd71534c8f69678bff7bc2939a561
Sugam <sharma_sugam@live.com>
2023-03-15 12:31:05 -0400

    Move Posts to local storage Add Compose Button to Nav Bar Modify Style

--- app.js
+++ app.js
@@ -3,19 +3,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const fs = require('fs');
 var _ = require('lodash');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome to Sugam Sharma's personal blog. I write each day about the things I learned, experienced, realized or dealt with. There is no day that goes by without teaching anything. Some Days I too get lazy, but I try to be punctual. Some Posts are smaller and some are lengthier than others. It's because some days are very happening, and some days are not happening at all. Some days you go by your daily routine and nothing extraordinary happens. Some days are so overwhelming that you don't even realize the day passing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-let posts = [];
+// Read the existing posts from the file
+const posts = JSON.parse(fs.readFileSync('posts.json'));
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 
 
 
@@ -39,6 +42,8 @@ app.post("/compose", function (req, res) {
     content: req.body.postBody
   };
   posts.push(post);
+  // Save the updated posts array back to the file
+  fs.writeFileSync('posts.json', JSON.stringify(posts));
   res.redirect("/");
 });
 app.get("/posts/:postTitle", function (req, res) {
@@ -47,7 +52,6 @@ app.get("/posts/:postTitle", function (req, res) {
     if (requestedTitle === _.lowerCase(post.title))
       res.render("post", { post: post });
   })
-
 })
 
 
